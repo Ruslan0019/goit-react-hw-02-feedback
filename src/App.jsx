633ba8c96290d6bdbb5a30c1be93726e9c0217fd
Ruslan0019 <133685041+Ruslan0019@users.noreply.github.com>
2024-01-17 17:08:02 +0200

    2

--- src/App.jsx
+++ src/App.jsx
@@ -9,35 +9,12 @@ export class App extends Component {
     good: 0,
     neutral: 0,
     bad: 0,
-    total: 0,
-    feedback: 0,
   };
 
-  handleClick = () => {
-    this.setState(
-      prevState => ({
-        good: prevState.good + 1,
-      }),
-      this.updateFeedbackStats
-    );
-  };
-
-  handleClick1 = () => {
-    this.setState(
-      prevState => ({
-        neutral: prevState.neutral + 1,
-      }),
-      this.updateFeedbackStats
-    );
-  };
-
-  handleClick2 = () => {
-    this.setState(
-      prevState => ({
-        bad: prevState.bad + 1,
-      }),
-      this.updateFeedbackStats
-    );
+  handleClick = feedbackType => {
+    this.setState(prevState => ({
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
   };
 
   countTotalFeedback = () => {
@@ -51,26 +28,18 @@ export class App extends Component {
     return total === 0 ? 0 : Math.floor((good / total) * 100);
   };
 
-  updateFeedbackStats = () => {
-    const total = this.countTotalFeedback();
-    const feedbackPercentage = this.countPositiveFeedbackPercentage();
-
-    this.setState({
-      total,
-      feedback: feedbackPercentage.toFixed(2),
-    });
-  };
-
   render() {
-    const { good, neutral, bad, total, feedback } = this.state;
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const feedback = this.countPositiveFeedbackPercentage();
+    const options = ['good', 'neutral', 'bad'];
 
     return (
       <>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            handleClick={this.handleClick}
-            handleClick1={this.handleClick1}
-            handleClick2={this.handleClick2}
+            options={options}
+            onLeaveFeedback={this.handleClick}
           />
         </Section>
         <Section title="Statistics">
